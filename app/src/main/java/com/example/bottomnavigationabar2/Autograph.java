c0219d51906631d847aa47dd28884bd0b2efd1ec
xuzzhihzong <210589563@qq.com>
2019-10-24 20:27:38 +0800

    个人信息分页面--许志中

---
+++ app/src/main/java/com/example/bottomnavigationabar2/Autograph.java
@@ -0,0 +1,32 @@
+package com.example.bottomnavigationabar2;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Autograph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_name);
+        ImageView imageView=(ImageView)findViewById(R.id.autograph_return);
+        TextView textView=(TextView)findViewById(R.id.autograph_sure);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/bottomnavigationabar2/Hobby.java
@@ -0,0 +1,32 @@
+package com.example.bottomnavigationabar2;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Hobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_name);
+        ImageView imageView=(ImageView)findViewById(R.id.hobby_return);
+        TextView textView=(TextView)findViewById(R.id.hobby_sure);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
