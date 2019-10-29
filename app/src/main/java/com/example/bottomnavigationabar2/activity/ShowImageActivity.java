3f6fe441409fc4ec83c8f34af5e111a1255f12c4
brokes6 <619199091@qq.com>
2019-10-29 21:49:23 +0800

    图片查看页面 美化 --付鑫博

--- app/src/main/java/com/example/bottomnavigationabar2/activity/ShowImageActivity.java
+++ app/src/main/java/com/example/bottomnavigationabar2/activity/ShowImageActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -72,7 +73,11 @@ public class ShowImageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.show_image_layout);
         initView();
         initData();
