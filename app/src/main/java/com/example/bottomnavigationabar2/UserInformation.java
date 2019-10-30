76bcefe8d714176b2617390e8f77353693093344
yuzhengyang666 <yzyyzy0524@qq.com>
2019-10-30 16:17:36 +0800

    基本用户信息页面（基本完成,补充细节） --余政洋

---
+++ app/src/main/java/com/example/bottomnavigationabar2/UserInformation.java
@@ -0,0 +1,13 @@
+package com.example.bottomnavigationabar2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_information);
+    }
+}
