88fc85d7ef6559a20e573c5c02046e58ae596569
brokes6 <619199091@qq.com>
2019-11-05 19:50:00 +0800

    给图片切换添加了动画                      --付鑫博

---
+++ app/src/main/java/com/example/bottomnavigationabar2/Animation/DepthPageTransformer.java
@@ -0,0 +1,44 @@
+package com.example.bottomnavigationabar2.Animation;
+
+/**
+ * 创建于2019/11/5 19:44🐎
+ */
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+//另外Google文档中还提供了另外一个动画的实现方式，我暂且把源代码附在以下：
+public class DepthPageTransformer implements ViewPager.PageTransformer {
+    private static final float MIN_SCALE = 0.75f;
+
+    public void transformPage(View view, float position) {
+        int pageWidth = view.getWidth();
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setAlpha(0);
+
+        } else if (position <= 0) { // [-1,0]
+            // Use the default slide transition when moving to the left page
+            view.setAlpha(1);
+            view.setTranslationX(0);
+            view.setScaleX(1);
+            view.setScaleY(1);
+
+        } else if (position <= 1) { // (0,1]
+            // Fade the page out.
+            view.setAlpha(1 - position);
+
+            // Counteract the default slide transition
+            view.setTranslationX(pageWidth * -position);
+
+            // Scale the page down (between MIN_SCALE and 1)
+            float scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - Math.abs(position));
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setAlpha(0);
+        }
+    }
+}
--- app/src/main/java/com/example/bottomnavigationabar2/Set_up.java
+++ app/src/main/java/com/example/bottomnavigationabar2/Set_up.java
@@ -1,13 +1,40 @@
 package com.example.bottomnavigationabar2;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bottomnavigationabar2.adapter.Setup_top;
 
 public class Set_up extends AppCompatActivity {
-
+        private TextView user_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_up);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //修改为深色，因为我们把状态栏的背景色修改为主题色白色，默认的文字及图标颜色为白色，导致看不到了。
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        //获取参数
+        user_back = findViewById(R.id.user_back);
+        user_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Set_up.this,"已退出",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Set_up.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
