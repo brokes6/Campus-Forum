08f222a2a897420004847be32f9a8d759a2c957b
xuzzhihzong <210589563@qq.com>
2019-12-04 09:32:29 +0800

    更新了关于我们 --许志中

--- app/src/main/java/com/example/bottomnavigationabar2/view/about_us.java
+++ app/src/main/java/com/example/bottomnavigationabar2/view/about_us.java
@@ -1,7 +1,11 @@
 package com.example.bottomnavigationabar2.view;
 
+import android.media.Image;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.bottomnavigationabar2.R;
 
@@ -11,5 +15,19 @@ public class about_us extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_us);
+        initView();
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //修改为深色，因为我们把状态栏的背景色修改为主题色白色，默认的文字及图标颜色为白色，导致看不到了。
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+    }
+    private void initView(){
+        ImageView image=(ImageView)findViewById(R.id.about_return);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
